5ac726e249985ae2c571ee4faa6bfcc9f4be1c16
Laurent Tettoni <laurent.tettoni@gmail.com>
2013-09-28 00:09:04 +0200

    Comments

--- src/main/java/org/logic2j/core/impl/DefaultSolver.java
+++ src/main/java/org/logic2j/core/impl/DefaultSolver.java
@@ -57,6 +57,8 @@ public class DefaultSolver implements Solver {
      */
     @Override
     public Continuation solveGoal(final Bindings theGoalBindings, final SolutionListener theSolutionListener) {
+        // Check if we will have to deal with DataFacts in this session of solving.
+        // This slightly improves performance - we can bypass calling the mehod that deals with that
         this.hasDataFactProviders = this.prolog.getTheoryManager().hasDataFactProviders();
         return solveGoalRecursive(theGoalBindings.getReferrer(), theGoalBindings, theSolutionListener);
     }
