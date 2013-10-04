75f0ba00f614d903f9d5348ef09a738f13184e09
Laurent Tettoni <laurent.tettoni@gmail.com>
2013-10-05 00:39:02 +0200

    Remove 2 @Ignore

--- src/test/java/org/logic2j/core/functional/AdapterTest.java
+++ src/test/java/org/logic2j/core/functional/AdapterTest.java
@@ -18,19 +18,15 @@
 package org.logic2j.core.functional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
-import java.awt.Button;
-import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.logic2j.core.PrologTestBase;
 import org.logic2j.core.api.Prolog;
 import org.logic2j.core.api.Solver;
-import org.logic2j.core.api.model.symbol.Struct;
-import org.logic2j.core.api.model.symbol.Term;
-import org.logic2j.core.api.solver.holder.MultipleSolutionsHolder;
+import org.logic2j.core.api.TermAdapter;
 
 /**
  * Test how Java objects can invoke and be returned from logic2j's {@link Prolog} and {@link Solver}.
@@ -50,36 +46,23 @@ public class AdapterTest extends PrologTestBase {
         public abstract Iterable<Out> solve(In in);
     }
 
-    @Ignore("Under design")
     @Test
-    public void invokeWithJavaObjects() throws Exception {
-        // Just convert a vararg of Object... to a Struct, and that's OK
-        this.prolog.solve(invocation("goal", 123, "str", 3.14, MyEnum.V1, new Object(), new Button()));
-        // ^^^static : en tout cas pas.
-    }
-
-    @Ignore("Under design")
-    @Test
-    public void testRetrieveJavaObjects() throws Exception {
-        this.prolog.solve("X is 2+3").unique().binding("X", Integer.class);
-    }
-
-    /**
-     * @param dataSetName
-     * @param args
-     * @return
-     */
-    private Term invocation(String predicateName, Object... args) {
-        return Struct.valueOf(predicateName, Arrays.asList(args));
+    public void retrieveJavaObjects() throws Exception {
+        assertEquals(new Long(5), this.prolog.solve("X is 2+3").unique().binding("X", Long.class));
     }
 
     @Test
     public void plainDouble() throws Exception {
         loadTheoryFromTestResourcesDir("test-functional.pl");
         //
-        MultipleSolutionsHolder solutions;
-        solutions = this.prolog.solve("dbl(X)").all();
-        final List<Object> binding = solutions.binding("X");
+        final List<Object> binding = this.prolog.solve("dbl(X)").all().binding("X");
         assertEquals("[1.1, 1.2, 1.3]", binding.toString());
+    }
+
+    @Test
+    public void javaEnum() throws Exception {
+        Object term = this.prolog.getTermAdapter().term("=", TermAdapter.FactoryMode.ANY_TERM, "X", MyEnum.V2);
+        final Object binding = this.prolog.solve(term).unique().binding("X");
+        assertSame(MyEnum.V2, binding);
     }
 }
