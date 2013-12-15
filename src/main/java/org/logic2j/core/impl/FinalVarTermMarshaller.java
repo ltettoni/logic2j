608ebacf9dee4f2b6389c070becb1b95ac7fb8f5
Laurent Tettoni <laurent.tettoni@gmail.com>
2013-12-16 00:06:38 +0100

    New

---
+++ src/main/java/org/logic2j/core/impl/FinalVarTermMarshaller.java
@@ -0,0 +1,67 @@
+/*
+ * logic2j - "Bring Logic to your Java" - Copyright (C) 2011 Laurent.Tettoni@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.logic2j.core.impl;
+
+import org.logic2j.core.api.model.symbol.TermApi;
+import org.logic2j.core.api.model.symbol.Var;
+import org.logic2j.core.api.model.var.Binding;
+import org.logic2j.core.api.model.var.Bindings;
+
+/**
+ * Like {@link DefaultTermMarshaller} but for every free var, report the variable at the end of
+ * bound variables chain.
+ */
+public class FinalVarTermMarshaller extends DefaultTermMarshaller {
+
+    private final Bindings bindings;
+
+    /**
+     * @param theProlog
+     * @param theBindingsForCriteria
+     */
+    public FinalVarTermMarshaller(PrologImplementation theProlog, Bindings theBindingsForCriteria) {
+        super(theProlog);
+        this.bindings = theBindingsForCriteria;
+    }
+
+    @Override
+    public CharSequence marshall(Object theTerm) {
+        if (theTerm instanceof Bindings) {
+            final Bindings b = (Bindings) theTerm;
+            return TermApi.accept(this, b.getReferrer(), b);
+        }
+        return TermApi.accept(this, theTerm, null);
+    }
+
+    @Override
+    public String visit(Var theVar, Bindings theBindings) {
+        if (theVar.isAnonymous()) {
+            return Var.ANONYMOUS_VAR_NAME;
+        }
+        final Binding initialBinding = theVar.bindingWithin(this.bindings);
+        final Binding finalBinding = initialBinding.followLinks();
+        final String formatted;
+        if (finalBinding.isFree()) {
+            formatted = finalBinding.getVar().getName();
+        } else {
+            formatted = TermApi.accept(this, finalBinding.getTerm(), theBindings);
+        }
+        return formatted;
+    }
+}
