0377b6ed14a0dad72a2ec832274aa01992822d5e
Laurent Tettoni <laurent.tettoni@gmail.com>
2014-06-06 22:43:41 +0200

    New builder acts as factory and integration to Spring.

---
+++ src/main/java/org/logic2j/core/library/impl/PrologBuilder.java
@@ -0,0 +1,131 @@
+package org.logic2j.core.library.impl;
+
+import org.logic2j.core.api.Prolog;
+import org.logic2j.core.api.model.exception.PrologNonSpecificError;
+import org.logic2j.core.impl.PrologReferenceImplementation;
+import org.logic2j.core.impl.theory.TheoryContent;
+import org.logic2j.core.impl.theory.TheoryManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * A builder to instantiate a Prolog implementation ready to be used.
+ * This object helps with the following tasks:
+ * - selecting the implementation to use
+ * - parameterize
+ * - configure the libraries (none, default, or extensions)
+ * - load theoryFiles
+ * <p/>
+ * This object is designed to be used either programmatically with its fluent API,
+ * or via CDI configuration since it is also a Java Bean whose properties can be injected.
+ * See http://stackoverflow.com/questions/2901166/how-to-make-spring-accept-fluent-non-void-setters
+ * regarding how to have both a Spring compatible Java Bean and a fluent API...
+ */
+public class PrologBuilder {
+
+    private boolean noLibraries = false;
+
+    private boolean coreLibraries = false;
+
+    private Collection<File> theoryFiles = new ArrayList<File>();
+    private Collection<String> theoryResources = new ArrayList<String>();
+
+    public Prolog createInstance() {
+        final PrologReferenceImplementation.InitLevel initLevel;
+        if (isNoLibraries()) {
+            initLevel = PrologReferenceImplementation.InitLevel.L0_BARE;
+        } else if (isCoreLibraries()) {
+            initLevel = PrologReferenceImplementation.InitLevel.L1_CORE_LIBRARY;
+        } else {
+            initLevel = PrologReferenceImplementation.InitLevel.L2_BASE_LIBRARIES;
+        }
+        final PrologReferenceImplementation prolog = new PrologReferenceImplementation(initLevel);
+
+
+        // Theories from files
+        final TheoryManager theoryManager = prolog.getTheoryManager();
+        try {
+            for (File theory : theoryFiles) {
+                final TheoryContent content = theoryManager.load(theory);
+                theoryManager.addTheory(content);
+            }
+        } catch (IOException e) {
+            throw new PrologNonSpecificError("Builder could not load theory: " + e);
+        }
+        // Theories from resources
+        for (String resource : theoryResources) {
+            final TheoryContent content = theoryManager.load(resource);
+            theoryManager.addTheory(content);
+        }
+
+        return prolog;
+    }
+
+
+    // ---------------------------------------------------------------------------
+    // Fluent API
+    // ---------------------------------------------------------------------------
+
+    public PrologBuilder withoutLibraries(boolean noLibraries) {
+        this.noLibraries = noLibraries;
+        return this;
+    }
+
+    public PrologBuilder withCoreLibraries(boolean coreLibraries) {
+        this.coreLibraries = coreLibraries;
+        return this;
+    }
+
+
+    public PrologBuilder withTheory(File... files) {
+        for (File file : files) {
+            theoryFiles.add(file);
+        }
+        return this;
+    }
+
+    public PrologBuilder withTheory(String... resources) {
+        for (String resource : resources) {
+            theoryResources.add(resource);
+        }
+        return this;
+    }
+    // ---------------------------------------------------------------------------
+    // Accessors
+    // ---------------------------------------------------------------------------
+
+    public boolean isNoLibraries() {
+        return noLibraries;
+    }
+
+    public void setNoLibraries(boolean noLibraries) {
+        this.noLibraries = noLibraries;
+    }
+
+    public boolean isCoreLibraries() {
+        return coreLibraries;
+    }
+
+    public void setCoreLibraries(boolean coreLibraries) {
+        this.coreLibraries = coreLibraries;
+    }
+
+    public Collection<File> getTheoryFiles() {
+        return theoryFiles;
+    }
+
+    public void setTheoryFiles(Collection<File> theoryFiles) {
+        this.theoryFiles = theoryFiles;
+    }
+
+    public Collection<String> getTheoryResources() {
+        return theoryResources;
+    }
+
+    public void setTheoryResources(Collection<String> theoryResources) {
+        this.theoryResources = theoryResources;
+    }
+}
---
+++ src/test/java/org/logic2j/core/library/impl/UseCaseTest.java
@@ -0,0 +1,56 @@
+package org.logic2j.core.library.impl;
+
+import org.junit.Test;
+import org.logic2j.core.api.Prolog;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Demonstrate simple use cases for Logic2j.
+ */
+public class UseCaseTest {
+
+    @Test
+    public void instantiateViaFactory() throws Exception {
+        final Prolog prolog = new PrologBuilder().createInstance();
+        assertNotNull(prolog);
+    }
+
+
+    @Test
+    public void configureDefaultLibs() throws Exception {
+        assertFalse(new PrologBuilder().withoutLibraries(false).isNoLibraries());
+        assertTrue(new PrologBuilder().withoutLibraries(true).isNoLibraries());
+        assertFalse(new PrologBuilder().withCoreLibraries(false).isCoreLibraries());
+        assertTrue(new PrologBuilder().withCoreLibraries(true).isCoreLibraries());
+    }
+
+    @Test
+    public void obtainDefaultPrologAndSolve() throws Exception {
+        final Prolog prolog = new PrologBuilder().createInstance();
+        assertEquals(4, prolog.solve("member(X, [a,b,c,d])").count());
+    }
+
+
+    @Test
+    public void loadTheoryAndSolve() throws Exception {
+        final File th1 = new File("src/test/resources/queens.pl");
+        final File th2 = new File("src/test/resources/hanoi.pl");
+        ;
+        final Prolog prolog = new PrologBuilder().withTheory(th1, th2).createInstance();
+        assertEquals(2, prolog.solve("queens(4, _)").count());
+    }
+
+
+    @Test
+    public void solve() throws Exception {
+        final Prolog prolog = new PrologBuilder()
+        .withTheory(new File("src/test/resources/queens.pl"))
+        .createInstance();
+        final List<Object> objectList = prolog.solve("queens(4, Q)").var("Q").list();
+        assertEquals(2, objectList);
+    }
+}
