db0815308a34db07c2823e8ea559702663cfda72
tettoni <laurent.tettoni@gmail.com>
2014-07-28 23:02:42 +0200

    New interface PrologBuilder

---
+++ src/main/java/org/logic2j/contrib/helper/PrologBuilder.java
@@ -0,0 +1,12 @@
+package org.logic2j.contrib.helper;
+
+import org.logic2j.core.api.Prolog;
+
+/**
+ * Instantiate a Prolog implementation.
+ */
+public interface PrologBuilder {
+
+  public Prolog createInstance();
+
+}
