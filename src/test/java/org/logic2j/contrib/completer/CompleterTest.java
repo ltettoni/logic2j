41a60323240216a1c59c8ccdd3525c60a0124cea
Laurent Tettoni <tettoni@iso.org>
2018-03-22 22:51:34 +0100

    Cleanup2

--- src/test/java/org/logic2j/contrib/completer/CompleterTest.java
+++ src/test/java/org/logic2j/contrib/completer/CompleterTest.java
@@ -22,13 +22,10 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.logic2j.core.PrologTestBase;
 import org.logic2j.core.impl.PrologReferenceImplementation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CompleterTest extends PrologTestBase {
-    private static final Logger logger = LoggerFactory.getLogger(CompleterTest.class);
 
     @Override
     protected PrologReferenceImplementation.InitLevel initLevel() {
