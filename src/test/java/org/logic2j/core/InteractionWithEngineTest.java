e8a0216c0a2813a53a243a504230dc56009741b4
tettoni <tettoni@iso.org>
2020-07-23 15:57:43 +0200

    Testing interaction with the engine's FOPredicate

---
+++ src/test/java/org/logic2j/core/InteractionWithEngineTest.java
@@ -0,0 +1,48 @@
+/*
+ * logic2j - "Bring Logic to your Java" - Copyright (c) 2017 Laurent.Tettoni@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.logic2j.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.logic2j.engine.model.SimpleBindings.bind;
+
+import org.junit.Test;
+import org.logic2j.engine.model.TermApi;
+import org.logic2j.engine.predicates.impl.math.compare.LT;
+import org.logic2j.engine.solver.listener.CountingSolutionListener;
+
+/**
+ * Check how the interaction is done with the lower-level logic2j-engine features.
+ */
+public class InteractionWithEngineTest extends PrologTestBase {
+  private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(InteractionWithEngineTest.class);
+
+  @Test
+  public void testUsingFOPredicate() {
+    Object term = new LT(bind(3), bind(4));
+    term = new TermApi().normalize(term);
+    final CountingSolutionListener listener = new CountingSolutionListener();
+    getProlog().getSolver().solveGoal(term, listener);
+    assertThat(listener.count()).as("Solving term \"" + term + '"').isEqualTo(1);
+  }
+
+  @Test
+  public void fromTheory() {
+    loadTheoryFromTestResourcesDir("test-engine.pro");
+    countOneSolution("twoLessThanThree");
+  }
+
+}
---
+++ src/test/resources/test-engine.pro
@@ -0,0 +1,5 @@
+/*
+        Testing the leverage of the low-level logic2j-engine features
+*/
+
+twoLessThanThree :- 2 < 3.
